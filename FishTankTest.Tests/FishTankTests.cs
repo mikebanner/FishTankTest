653165fb02575500399516d5eb48a39e817b159d
Mike Banner <mike@leafone.co.uk>
2023-09-07 11:47:47 +0100

    test: Failing test for angelfish weight given multiple angel fish

--- FishTankTest.Tests/FishTankTests.cs
+++ FishTankTest.Tests/FishTankTests.cs
@@ -31,4 +31,22 @@ public class FishTankTests
         
         Assert.Equal(expectedWeight, result);
     }
+    
+    [Theory]
+    [InlineData(1, 0.2)]
+    [InlineData(2, 0.4)]
+    [InlineData(20, 4.0)]
+    public void FishTankWithMultipleAngelfishReturnsCorrectFeedWeight(int numberOfFish, decimal expectedWeight)
+    {
+        var sut = new FishTank();
+
+        for (int i = 0; i < numberOfFish; i++)
+        {
+            sut.AddFish(new Goldfish($"Angelica {i}"));
+        }
+
+        var result = sut.Feed();
+        
+        Assert.Equal(expectedWeight, result);
+    }
 }
