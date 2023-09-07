36010163e500f9aabca74dddc375856a4c0b0a50
Mike Banner <mike@leafone.co.uk>
2023-09-07 11:07:25 +0100

    test: initial failing test for empty tank

--- FishTankTest.Tests/FishTankTests.cs
+++ FishTankTest.Tests/FishTankTests.cs
@@ -1,9 +1,16 @@
+using NuGet.Frameworks;
+
 namespace FishTankTest.Tests;
 
 public class FishTankTests
 {
     [Fact]
-    public void Test1()
+    public void EmptyFishTankReturnsZeroWeightOfFeed()
     {
+        var sut = new FishTank();
+
+        var result = sut.Feed();
+        
+        Assert.Equal(0, result);
     }
 }
